0a4d0af13ee16febab26b0b2b7741f0d6e3d608c
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-01-26 18:15:49 -0300

    criação do DB - tabela albuns

--- desafio1.sql
+++ desafio1.sql
@@ -13,6 +13,14 @@ DROP DATABASE IF EXISTS SpotifyClone;
       valor_plano DOUBLE NOT NULL,
   ) engine = InnoDB;
 
+      CREATE TABLE SpotifyClone.albuns(
+      album_id INT PRIMARY KEY AUTO_INCREMENT,
+      album_nome VARCHAR(250) NOT NULL,
+      ano_lancamento YEAR NOT NULL,
+      artista_id INT NOT NULL
+      FOREIGN KEY (artista_id) REFERENCES artistas (artista_id)
+  ) engine = InnoDB;
+
   INSERT INTO SpotifyClone.artistas (artista_nome)
   VALUES
     ('Beyoncé'),
@@ -28,3 +36,14 @@ DROP DATABASE IF EXISTS SpotifyClone;
     ('familiar', 7.99);
     ('universitário', 5.99);
     ('universitário', 6.99);
+
+  INSERT INTO SpotifyClone.albuns (album_nome, ano_lancamento, artista_id)
+  VALUES
+    ('Renaissance', 2022, 1),
+    ('Jazz', 1978, 2),
+    ('Hot Space', 1982, 2),
+    ('Falso Brilhante', 1998, 3),
+    ('Vento de Maio', 2001, 3),
+    ('QVVJFA?', 2003, 4);
+    ('Somewhere Far Beyond', 2007, 5);
+    ('I Put A Spell On You', 2012, 6);
